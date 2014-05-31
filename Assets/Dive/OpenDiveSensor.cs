a74712e7c2e3980e1a9aef137eb4cdca85f30109
libsora.so <libsora25@gmail.com>
2014-05-31 11:03:48 +0900

    dive sdk 코드 일부 수정

--- Assets/Dive/OpenDiveSensor.cs
+++ Assets/Dive/OpenDiveSensor.cs
@@ -6,30 +6,11 @@ using System.Runtime.InteropServices;
 
 //Durovis Dive Head Tracking 
 // copyright by Shoogee GmbH & Co. KG Refer to LICENCE.txt 
-
-
+//OpenDive HEad Tracking for Unity by Stefan Welker
 
 public class OpenDiveSensor : MonoBehaviour {
-	
-	// This is used for rotating the camera with another object
-	//for example tilting the camera while going along a racetrack or rollercoaster
-
-	
-	public bool add_rotation_gameobject=false;
-	
-	// add the head of the player to enable tilting by gameobject
-	public GameObject rotation_gameobject;
-	public Texture nogyrotexture;
-	private float q0,q1,q2,q3;
-	Quaternion rot;
-	private bool show_error_message;
-
-	string errormessage;
-
 #if UNITY_EDITOR
-
-  #elif UNITY_ANDROID
-
+#elif UNITY_ANDROID
 	[DllImport("divesensor")]	private static extern void initialize_sensors();
 	[DllImport("divesensor")]	private static extern int get_q(ref float q0,ref float q1,ref float q2,ref float q3);
 	[DllImport("divesensor")]	private static extern int process();
@@ -38,71 +19,50 @@ public class OpenDiveSensor : MonoBehaviour {
 	[DllImport("divesensor")]	private static extern void get_version(string msg,int maxlength);
 	[DllImport("divesensor")]	private static extern int get_error();
 
-
-
-   
-   #elif UNITY_IPHONE
+#elif UNITY_IPHONE
 	[DllImport("__Internal")]	private static extern void initialize_sensors();
 	[DllImport("__Internal")]	private static extern float get_q0();
 	[DllImport("__Internal")]	private static extern float get_q1();
 	[DllImport("__Internal")]	private static extern float get_q2();
 	[DllImport("__Internal")]	private static extern float get_q3();
 	[DllImport("__Internal")]	private static extern void DiveUpdateGyroData();
-    [DllImport("__Internal")]	private static extern int get_q(ref float q0,ref float q1,ref float q2,ref float q3);
-	
-	
-#endif 	
+	[DllImport("__Internal")]	private static extern int get_q(ref float q0,ref float q1,ref float q2,ref float q3);
+#endif
 
-	void Start () {
 
-		show_error_message=true;
-
-		rot=Quaternion.identity;
-	    // Disable screen dimming
-     	Screen.sleepTimeout = SleepTimeout.NeverSleep;
+	void Start() {
+		// Disable screen dimming
+		Screen.sleepTimeout = SleepTimeout.NeverSleep;
 		Application.targetFrameRate = 60;
 
-
-
-
-
-
 #if UNITY_EDITOR
-  #elif UNITY_ANDROID
+#elif UNITY_ANDROID
 		Network.logLevel = NetworkLogLevel.Full;
 		use_udp(1);
 		initialize_sensors ();
-		int err = get_error();
-		if (err==0){ errormessage="";
-			show_error_message=false;
-
-		}
-		if (err==1){
-			show_error_message=true;
-			errormessage="ERROR: Dive needs a Gyroscope and your telephone has none, we are trying to go to Accelerometer compatibility mode. Dont expect too much.";
-		}
-
-
-	#elif UNITY_IPHONE
+		//int err = get_error();
+#elif UNITY_IPHONE
 		initialize_sensors();
 #endif
-		
 	}
 	
-	
-	void Update () {
 
-		
+	void Update() {
 #if UNITY_EDITOR
-
-	#elif UNITY_ANDROID
-
+#elif UNITY_ANDROID
+		float q0 = 0, q1 = 0, q2 = 0, q3 = 0;
 		process();
+
+		Quaternion rot;
 		get_q(ref q0,ref q1,ref q2,ref q3);
 		rot.x=-q2;rot.y=q3;rot.z=-q1;rot.w=q0;
-		#elif UNITY_IPHONE
+		transform.rotation = rot;
+#elif UNITY_IPHONE
+		float q0 = 0, q1 = 0, q2 = 0, q3 = 0;
 		DiveUpdateGyroData();
 		get_q(ref q0,ref q1,ref q2,ref q3);
+
+		Quaternion rot;
 		rot.x=-q2;
 		rot.y=q3;
 		rot.z=-q1;
@@ -110,31 +70,9 @@ public class OpenDiveSensor : MonoBehaviour {
 		transform.rotation = rot;
 #endif
 
-		if (add_rotation_gameobject){
-						transform.rotation =rotation_gameobject.transform.rotation* rot;
-		}
-		else
-		{
-						transform.rotation = rot;
-		}
-	
-		
-
-
 	}
-	
-	void OnGUI ()
-	{
-		//GUI.Label(new Rect(100,100,400,100),errormessage);
-		if (show_error_message)
-		{
-			if(GUI.Button(new Rect(0,0, Screen.width, Screen.height) , "button")) {
-				show_error_message=false;
-			}
-			GUI.DrawTexture(new Rect(Screen.width/2-320, Screen.height/2-240, 640, 480), nogyrotexture, ScaleMode.ScaleToFit, true, 0);
 
-
-		}
-
+	void OnGUI() {
 	}
 }
+
